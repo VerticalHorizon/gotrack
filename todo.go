0cbb6a97da967d2fb2aa1b02e8d3f3e64708684b
Denis Bozhenkov <mastermind@karellens.com>
2017-10-23 17:53:00 +0300

    simple todo rest service

---
+++ todo.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+
+type Todo struct {
+    Name        string      `json:"name"`
+    Completed   string      `json:"completed"`
+    Due         time.Time   `json:"due"`
+}
+
+type Todos []Todo
